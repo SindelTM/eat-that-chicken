b915d516bf5eafbedfe881e2f6dee32b7a5b21ba
Mariana Naidenova <mariananaidenova@mail.bg>
2016-06-19 13:19:17 +0300

    Added Birds folder and Birds class initial commit

---
+++ EatThatChicken/EatThatChicken/GameObjects/Birds/Bird.cs
@@ -0,0 +1,12 @@
+﻿namespace EatThatChicken.GameObjects.Birds
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Bird : GameObject
+    {
+    }
+}
