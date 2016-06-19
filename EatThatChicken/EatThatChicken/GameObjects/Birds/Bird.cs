30a2a869001e7b04e677936ff48c29c66f9ca57c
Mariana Naidenova <mariananaidenova@mail.bg>
2016-06-19 17:25:43 +0300

    Added class Bird and initial commit of turkeys

--- EatThatChicken/EatThatChicken/GameObjects/Birds/Bird.cs
+++ EatThatChicken/EatThatChicken/GameObjects/Birds/Bird.cs
@@ -1,12 +1,33 @@
 ﻿namespace EatThatChicken.GameObjects.Birds
 {
     using System;
+    using Contracts;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
-    public class Bird : GameObject
+    public class Bird : GameObject, IMoveable, IUpdateable
     {
+        public Bird()
+            :base()
+        {
+        }
+
+        public Size Bounds { get; set; }
+
+        public Position Position { get; set; }
+
+        public virtual bool IsAlive { get; set; }
+
+        public void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Move()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
