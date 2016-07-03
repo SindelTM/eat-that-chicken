6b9a5ccd3c06a4c0baf11ecae8fee1257638c1a5
ludzhev <ludzhev@gmail.com>
2016-07-04 01:49:04 +0300

    HunterFactory

---
+++ EatThatChicken/EatThatChicken/Factories/HunterFactory.cs
@@ -0,0 +1,21 @@
+﻿using EatThatChicken.Contracts;
+using EatThatChicken.GameObjects;
+using EatThatChicken.GameObjects.Hunters;
+
+namespace EatThatChicken.Factories
+{
+    public class HunterFactory : IGameObjectFactory<Hunter>
+    {
+        const int HunterHeight = 190;
+        const int HunterWidth = 90;
+        const int HunterPoints = 100;
+
+        public Hunter Get(int screenHeight, int screenWidth)
+        {
+            Position position = new Position((screenWidth - HunterWidth)/2, (screenHeight - HunterHeight));
+            Size bounds = new Size(HunterWidth, HunterHeight);
+
+            return new Hunter(bounds, position);
+        }
+    }
+}
